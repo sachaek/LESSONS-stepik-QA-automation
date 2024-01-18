f62019dca32ae1e83841b2f3cb02d77f908330a9
sacha_ek <sacha-ek@yandex.ru>
2024-01-18 21:29:46 +0500

    Stepik, Selenium, 1.6.3 добавил keyboard.wait('esc'), конструкцию try/finally

--- submit_button.py
+++ submit_button.py
@@ -1,12 +1,17 @@
+import keyboard
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 link = "http://suninjuly.github.io/simple_form_find_task.html"
-browser = webdriver.Chrome()
-browser.get(link)
-button = browser.find_element(By.ID, "submit_button")
-button.click()
 
-# закрываем браузер после всех манипуляций
-browser.quit()
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button = browser.find_element(By.ID, "submit_button")
+    button.click()
+    keyboard.wait('esc')
+
+finally:
+    pass
+    # закрываем браузер после всех манипуляций
+    browser.quit()
