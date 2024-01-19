1b53134205c12e5ec9924eeba3ca2ad05dd84850
sacha_ek <sacha-ek@yandex.ru>
2024-01-19 13:26:02 +0500

    Stepik, Selenium, 1.6.5 выполнил задание

--- by_link_text.py
+++ by_link_text.py
@@ -3,14 +3,25 @@ from selenium.webdriver.common.by import By
 import time
 import math
 
-in_link = "https://www.degreesymbol.net/"
+in_link = 'http://suninjuly.github.io/find_link_text'
 browser = webdriver.Chrome()
 
 try:
     browser.get(in_link)
-    link = browser.find_element(By.LINK_TEXT, "Degree Symbol in Math")
+    link_text = str(math.ceil(math.pow(math.pi, math.e)*10000))
+    link = browser.find_element(By.LINK_TEXT, link_text)
     link.click()
+    input1 = browser.find_element(By.NAME, "first_name")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element(By.NAME, "last_name")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element(By.CLASS_NAME, "city")
+    input3.send_keys("Smolensk")
+    input4 = browser.find_element(By.ID, "country")
+    input4.send_keys("Russia")
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
 
 finally:
-    time.sleep(3)
+    time.sleep(5)
     browser.quit()
