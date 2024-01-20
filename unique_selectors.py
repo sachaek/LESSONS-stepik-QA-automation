0c9bbf2d0eaad126ccdbdbe59c780f78ed35e93b
sacha_ek <sacha-ek@yandex.ru>
2024-01-20 10:48:49 +0500

    Stepik, Selenium, 2.1.2 создал файл 2_radiobutton.py

--- unique_selectors.py
+++ unique_selectors.py
@@ -3,17 +3,17 @@ from selenium.webdriver.common.by import By
 import time
 
 try:
-    link = "http://suninjuly.github.io/registration1.html"
+    link = "http://suninjuly.github.io/registration2.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element(By.CSS_SELECTOR, "input.first")
+    input1 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']")
     input1.send_keys("Alex")
-    input2 = browser.find_element(By.CSS_SELECTOR, "input.second")
+    input2 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
     input2.send_keys("Surname")
-    input3 = browser.find_element(By.CSS_SELECTOR, "input.third")
+    input3 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
     input3.send_keys("sacha@mail.com")
-
+    time.sleep(1)
     # Отправляем заполненную форму
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
