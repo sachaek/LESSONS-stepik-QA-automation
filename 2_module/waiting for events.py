7894e69f665f87d4046c9cc43d450a58502fed04
sacha_ek <sacha-ek@yandex.ru>
2024-01-22 18:14:06 +0500

    Selenium, Stepik, 2.4.6 написал простой код с неявным ожиданием

--- 2_module/waiting for events.py
+++ 2_module/waiting for events.py
@@ -5,11 +5,8 @@ from selenium.webdriver.common.by import By
 
 browser = webdriver.Chrome()
 browser.implicitly_wait(5)
-browser.get("http://suninjuly.github.io/wait1.html")
-# time.sleep(3)
-button = browser.find_element(By.ID, "verify")
+browser.get("https://suninjuly.github.io/cats.html")
+button = browser.find_element(By.ID, "button")
 button.click()
-# time.sleep(3)
-message = browser.find_element(By.ID, "verify_message")
 
-assert "successful" in message.text
+browser.quit()
