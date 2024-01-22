0c1c6022a434fd26eb69ce737530c69ee7a058e0
sacha_ek <sacha-ek@yandex.ru>
2024-01-22 22:18:10 +0500

    Selenium, Stepik, 2.4.6 написал код с "ожиданиями".

--- 2_module/waiting for events.py
+++ 2_module/waiting for events.py
@@ -1,3 +1,6 @@
+import math
+import time
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -5,13 +8,32 @@ from selenium import webdriver
 
 browser = webdriver.Chrome()
 
-browser.get("http://suninjuly.github.io/wait2.html")
 
-# говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
-button = WebDriverWait(browser, 5).until(
-        EC.element_to_be_clickable((By.ID, "verify"))
-    )
-button.click()
-message = browser.find_element(By.ID, "verify_message")
+def calc(x: str) -> str:
+    return str(math.log(abs(12*math.sin(int(x)))))
 
-assert "successful" in message.text
+
+try:
+    browser.implicitly_wait(5)
+
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+
+    value = WebDriverWait(browser, 12).until(
+            EC.text_to_be_present_in_element((By.ID, "price"), "$100")
+        )
+
+    button = browser.find_element(By.CLASS_NAME, "btn-primary")
+    button.click()
+
+    value_x = browser.find_element(By.ID, "input_value").text
+    y = calc(value_x)
+
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+
+    button2 = browser.find_element(By.ID, "solve")
+    button2.click()
+
+finally:
+    time.sleep(7)
+    browser.quit()
