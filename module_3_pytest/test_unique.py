e5228d664e858f13efcd1347272df38af444b38a
sacha_ek <sacha-ek@yandex.ru>
2024-01-23 22:48:35 +0500

    Selenium, Stepik, 3.2.13 оптимизировал код, DRY

--- module_3_pytest/test_unique.py
+++ module_3_pytest/test_unique.py
@@ -19,17 +19,18 @@ def link_t(link):
     button.click()
     time.sleep(1)
     welcome_text = browser.find_element(By.TAG_NAME, "h1").text
+    print(welcome_text)
     return welcome_text
 
 
 class TestReg(unittest.TestCase):
     def test_reg1(self):
         self.assertEqual(link_t("http://suninjuly.github.io/registration1.html"),
-                         "Поздравляем! Вы успешно зарегистировались!", "registration is failed")
+                         'Congratulations! You have successfully registered!', "registration is failed")
 
     def test_reg2(self):
         self.assertEqual(link_t("http://suninjuly.github.io/registration2.html"),
-                         "Поздравляем! Вы успешно зарегистировались!", "registration is failed")
+                         'Congratulations! You have successfully registered!', "registration is failed")
 
 
 if __name__ == "__main__":
