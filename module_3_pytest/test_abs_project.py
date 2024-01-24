adf79b48ad2ee71b0bbe44860057bfe06be40e93
sacha_ek <sacha-ek@yandex.ru>
2024-01-24 10:15:55 +0500

    Selenium, Stepik, 3.3.9 INIT

--- module_3_pytest/test_abs_project.py
+++ module_3_pytest/test_abs_project.py
@@ -1,13 +1,8 @@
-import unittest
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
 
 
-class TestAbs(unittest.TestCase):
-    def test_abs1(self):
-        self.assertEquals(abs(-42), 42, "Should be absolute value of a number")
-
-    def test_abs2(self):
-        self.assertEquals(abs(-42), -42, "Should be absolute value of a number")
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
 
 
-if __name__ == "__main__":
-    unittest.main()
