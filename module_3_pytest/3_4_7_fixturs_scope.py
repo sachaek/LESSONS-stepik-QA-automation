50d08653592f798a50a96ef3f8303b211e664f18
sacha_ek <sacha-ek@yandex.ru>
2024-01-24 17:27:08 +0500

    Selenium, Stepik, 3.4.7 код, теория, добавил pass , чтоб не было ошибок IDE

--- module_3_pytest/3_4_7_fixturs_scope.py
+++ module_3_pytest/3_4_7_fixturs_scope.py
@@ -21,6 +21,8 @@ def print_smiling_faces():
 class TestPrintSmilingFaces():
     def test_first_smiling_faces(self, prepare_faces, very_important_fixture):
         # какие-то проверки
+        pass
 
     def test_second_smiling_faces(self, prepare_faces):
         # какие-то проверки
+        pass
